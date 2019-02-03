3b916fc76be58902a61faae54580023e885b46f4
Christopher Mohammed <christophermohammed11@outlook.com>
2019-02-03 15:10:20 -0500

    Removed all YellowBox warnings

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-console.ignoredYellowBox = ['Warning: You should only render'];
+console.disableYellowBox = true;
 
 AppRegistry.registerComponent(appName, () => App);
