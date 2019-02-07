a010e03a2e5c2d77f4875e74d23375431f338575
christophermohammed <christophermohammed11@outlook.com>
2019-02-07 00:31:01 -0500

    Fixed scrollview and flatlist bug | TEMPORARY

--- src/components/screens/Home/index.js
+++ src/components/screens/Home/index.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View, Button, StatusBar, ScrollView, FlatList, AsyncStorage, ActivityIndicator } from 'react-native';
+import { StyleSheet, Text, View, Button, StatusBar, ScrollView, FlatList, AsyncStorage, TouchableOpacity } from 'react-native';
 
 import NetSavingsCard from '../../netsavingsCard';
 import Tip from '../../tipoftheday';
@@ -16,10 +16,11 @@ class Home extends Component {
       user: {
         netSav: 0
       },
+      photos: [],
 
+      enableScrollViewScroll: true,
       refreshing: false,
       loading: false,
-      photos: []
     }
   }
 
@@ -45,48 +46,68 @@ class Home extends Component {
     this.setState({photos: photos});
   }
 
+  onEnableScroll = (value) => {
+    this.setState({enableScrollViewScroll: value});
+  };
+
   render() {
     return (
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <View>
-          <StatusBar
-            backgroundColor="white"
-            barStyle="dark-content"
-          />
-          {/* Net savings section */}
-          <View style={[styles.section, {marginTop: 0}]}>
-            <Text style={styles.title}>Savings</Text>
-            <View style={{height: 70}}>
-              <FlatList
-                data={[this.state.user]}
-                keyExtractor={(_item, index) => index.toString()}
-                renderItem={({item}) => 
-                  <View>
-                      <NetSavingsCard item={item}/>
-                  </View>
-                }
-                refreshing={this.state.refreshing}
-                onRefresh={this.refreshFlatList}
-                showsVerticalScrollIndicator={false}
-              />
+      <View>
+        <ScrollView 
+          showsVerticalScrollIndicator={false}
+          scrollEnabled={this.state.enableScrollViewScroll}
+        >
+          <View>
+            <StatusBar
+              backgroundColor="white"
+              barStyle="dark-content"
+            />
+            {/* Net savings section */}
+            <View style={[styles.section, {marginTop: 0}]}>
+              <Text style={styles.title}>Savings</Text>
+              <View style={{height: 70}}>
+                <FlatList
+                  data={[this.state.user]}
+                  keyExtractor={(_item, index) => index.toString()}
+                  renderItem={({item}) => 
+                    <View>
+                        <NetSavingsCard item={item}/>
+                    </View>
+                  }
+                  refreshing={this.state.refreshing}
+                  onRefresh={this.refreshFlatList}
+                  showsVerticalScrollIndicator={false}
+                  onTouchStart={() => {
+                    this.onEnableScroll(false);
+                  }}
+                  onMomentumScrollEnd={() => {
+                    this.onEnableScroll(true);
+                  }}
+                />
+              </View>
             </View>
+            <TouchableOpacity 
+              onPress={() => this.onEnableScroll(true)}
+              activeOpacity={1}
+            >
+              {/* Tip section */}
+              <View style={styles.section}>
+                <Text style={styles.title}>Tip of the day</Text>
+                <Tip />
+              </View>
+              {/* Gallery section */}
+              <View style={styles.section}>
+                <Text style={styles.title}>Gallery</Text>
+                <Carousel photos={this.state.photos}/>
+              </View>
+              {/*<Button 
+                title="Clear"
+                onPress={clearAsync}
+              />*/}
+            </TouchableOpacity>
           </View>
-          {/* Tip section */}
-          <View style={styles.section}>
-            <Text style={styles.title}>Tip of the day</Text>
-            <Tip />
-          </View>
-          {/* Gallery section */}
-          <View style={styles.section}>
-            <Text style={styles.title}>Gallery</Text>
-            <Carousel photos={this.state.photos}/>
-          </View>
-          {/*<Button 
-            title="Clear"
-            onPress={clearAsync}
-          />*/}
-        </View>
-      </ScrollView>
+        </ScrollView>
+      </View>
     );
   }
 }
