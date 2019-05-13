c522093d19bd35b89fdf73fbc57d6a8dfb025222
christophermohammed <christophermohammed11@outlook.com>
2019-05-13 17:44:18 -0400

    Works on android. Hope i didnt break ios

--- android/app/src/main/java/com/vings/MainApplication.java
+++ android/app/src/main/java/com/vings/MainApplication.java
@@ -8,6 +8,7 @@ import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 
 import com.facebook.react.ReactApplication;
+import com.wheelpicker.WheelPickerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new WheelPickerPackage(),
           new AsyncStoragePackage(),
           new NetInfoPackage(),
           new VectorIconsPackage(),
